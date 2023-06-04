d1522db39cc7552720b4e719b8963af7431b683d
Hoon Wee <mrgravity817@gmail.com>
2023-06-04 11:06:48 +0900

    test: renders avatar with given email

--- test/ranger_web/live/avatar_live_test.exs
+++ test/ranger_web/live/avatar_live_test.exs
@@ -1,5 +1,17 @@
 defmodule RangerWeb.AvatarLiveTest do
+  alias Ranger.Gravatar
   use RangerWeb.ConnCase
 
   import Phoenix.LiveViewTest
+
+  alias Ranger.Gravatar
+
+  test "renders avatar for given email", %{conn: conn} do
+    email = "hoon@mail.com"
+    avatar_url = Gravatar.generate(email)
+
+    {:ok, _view, html} = live(conn, ~p"/avatar/#{email}")
+
+    assert html =~ avatar_url
+  end
 end
