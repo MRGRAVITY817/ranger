66de86e95e79971208a841f5a44c35a665c281b1
Hoon Wee <mrgravity817@gmail.com>
2023-06-04 10:10:26 +0900

    test: upgrades the connection

--- test/ranger_web/live/greet_live_test.exs
+++ test/ranger_web/live/greet_live_test.exs
@@ -8,4 +8,12 @@ defmodule RangerWeb.GreetLiveTest do
 
     assert html_response(disconnected, 200) =~ "Welcome to stateless HTTP"
   end
+
+  test "upgrades the connection", %{conn: conn} do
+    disconnected = conn |> get(~p"/greet")
+    # upgrade the connection
+    {:ok, _view, html} = live(disconnected)
+
+    assert html =~ "Welcome to Testing LiveView"
+  end
 end
