98ee7d498806c0fc6b69439f456109ff125a034a
Hoon Wee <mrgravity817@gmail.com>
2023-06-04 11:11:17 +0900

    test: fragile test - renders avatar HTML

--- test/ranger_web/live/avatar_live_test.exs
+++ test/ranger_web/live/avatar_live_test.exs
@@ -14,4 +14,17 @@ defmodule RangerWeb.AvatarLiveTest do
 
     assert html =~ avatar_url
   end
+
+  # FRAGILE TEST: what if `class` is added, like tailwind utility classes?
+  # We should test only the core behavior - not the volatile parts.
+  test "renders avatar HTML", %{conn: conn} do
+    email = "hoon@mail.com"
+    avatar_url = Gravatar.generate(email)
+
+    {:ok, _view, html} = live(conn, ~p"/avatar/#{email}")
+
+    avatar = ~s(<img class="avatar" src="#{avatar_url}")
+
+    assert html =~ avatar
+  end
 end
