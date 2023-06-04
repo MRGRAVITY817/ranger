025c6220361c54b4b92e781915fb40ef6f26baac
Hoon Wee <mrgravity817@gmail.com>
2023-06-04 10:05:29 +0900

    test: renders disconnected state

--- test/ranger_web/live/greet_live_test.exs
+++ test/ranger_web/live/greet_live_test.exs
@@ -2,4 +2,10 @@ defmodule RangerWeb.GreetLiveTest do
   use RangerWeb.ConnCase
 
   import Phoenix.LiveViewTest
+
+  test "renders disconnected state", %{conn: conn} do
+    disconnected = conn |> get(~p"/greet")
+
+    assert html_response(disconnected, 200) =~ "Welcome to stateless HTTP"
+  end
 end
