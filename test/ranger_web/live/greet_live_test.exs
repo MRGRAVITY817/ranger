b69cd422d3d081638cccba9baf614b93f138f22a
Hoon Wee <mrgravity817@gmail.com>
2023-06-04 10:25:31 +0900

    test: renders connected state

--- test/ranger_web/live/greet_live_test.exs
+++ test/ranger_web/live/greet_live_test.exs
@@ -16,4 +16,11 @@ defmodule RangerWeb.GreetLiveTest do
 
     assert html =~ "Welcome to Testing LiveView"
   end
+
+  test "rendering connected state", %{conn: conn} do
+    # Start right away from connected state
+    {:ok, _view, html} = live(conn, ~p"/greet")
+
+    assert html =~ "Welcome to Testing LiveView"
+  end
 end
