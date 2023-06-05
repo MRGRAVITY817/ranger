43569ad82953fdc9434c1eac3a13a71bbe49a599
Hoon Wee <mrgravity817@gmail.com>
2023-06-05 22:32:11 +0900

    test: bad test - firing event without pressing button

--- test/ranger_web/live/counter_live_test.exs
+++ test/ranger_web/live/counter_live_test.exs
@@ -12,4 +12,14 @@ defmodule RangerWeb.CounterLiveTest do
 
     assert has_element?(view, "#count", "1")
   end
+
+  test "user can increase counter (target event directly)", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/counter")
+
+    # This fires 'increase()' event directly, without pressing the button
+    view |> render_click("increase")
+
+    # It would pass, but that's not what we want.
+    assert has_element?(view, "#count", "1")
+  end
 end
