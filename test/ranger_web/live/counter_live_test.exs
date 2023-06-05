b59bc2ef956a896ec723f6afa960cfe8788c56bb
Hoon Wee <mrgravity817@gmail.com>
2023-06-05 22:27:03 +0900

    test: add test if counter increases

--- test/ranger_web/live/counter_live_test.exs
+++ test/ranger_web/live/counter_live_test.exs
@@ -2,4 +2,14 @@ defmodule RangerWeb.CounterLiveTest do
   use RangerWeb.ConnCase
 
   import Phoenix.LiveViewTest
+
+  test "user can increase counter", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/counter")
+
+    view
+    |> element("#increment")
+    |> render_click()
+
+    assert has_element?(view, "#count", "1")
+  end
 end
