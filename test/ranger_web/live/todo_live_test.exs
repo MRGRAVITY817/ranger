7ac67e63843ec0039edac3d65e6254581ffe97fa
Hoon Wee <mrgravity817@gmail.com>
2023-06-05 23:40:24 +0900

    test: creating new todo item via form

--- test/ranger_web/live/todo_live_test.exs
+++ test/ranger_web/live/todo_live_test.exs
@@ -2,4 +2,14 @@ defmodule RangerWeb.TodoLiveTest do
   use RangerWeb.ConnCase
 
   import Phoenix.LiveViewTest
+
+  test "user can create a new todo", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/todo")
+
+    view
+    |> form("#add-todo", %{todo: %{body: "Form fellowship"}})
+    |> render_submit()
+
+    assert has_element?(view, "[data-role=todo]", "Form fellowship")
+  end
 end
