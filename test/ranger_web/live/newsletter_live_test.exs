994cf3666aebee6755db26a1b7dd65c894ae028d
Hoon Wee <mrgravity817@gmail.com>
2023-06-10 18:55:40 +0900

    test: test email validation by direct event firing

--- test/ranger_web/live/newsletter_live_test.exs
+++ test/ranger_web/live/newsletter_live_test.exs
@@ -9,8 +9,25 @@ defmodule RangerWeb.NewsletterLiveTest do
 
     html =
       view
-      |> form("#subscribe", %{subscription: %{email: invalid_email}})
+      |> form(
+        "#subscribe",
+        %{subscription: %{email: invalid_email}}
+      )
       |> render_change()
+
+    assert html =~ "has invalid format"
+  end
+
+  test "warns user of invalid email as user types (target event directly)", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/newsletter")
+    invalid_email = "something"
+
+    html =
+      view
+      |> render_change(
+        "validate",
+        %{subscription: %{email: invalid_email}}
+      )
 
     assert html =~ "has invalid format"
   end
